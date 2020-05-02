cbf11893c5e1d8d94105d8fd4015f04ae93b5345
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-02 15:48:19 +0200

    Added FK's in UserConfiguration

--- Zappr.Api/Data/Configurations/UserConfiguration.cs
+++ Zappr.Api/Data/Configurations/UserConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Zappr.Api.Domain;
 
@@ -12,13 +12,13 @@ namespace Zappr.Api.Data.Configurations
             builder.HasKey(u => u.Id);
 
             //Series
-            builder.HasMany(u => u.FavoriteSeries).WithOne();
-            builder.HasMany(u => u.WatchListedSeries).WithOne();
-            builder.HasMany(u => u.RatedSeries).WithOne();
+            builder.HasMany(u => u.FavoriteSeries).WithOne(ufs => ufs.User).HasForeignKey(u => u.UserId); ;
+            builder.HasMany(u => u.WatchListedSeries).WithOne(uws => uws.User).HasForeignKey(u => u.UserId); ;
+            builder.HasMany(u => u.RatedSeries).WithOne(urs => urs.User).HasForeignKey(u => u.UserId); ;
 
             //Episodes
-            builder.HasMany(u => u.RatedEpisodes).WithOne();
-            builder.HasMany(u => u.WatchedEpisodes).WithOne();
+            builder.HasMany(u => u.RatedEpisodes).WithOne(ure => ure.User).HasForeignKey(u => u.UserId);
+            builder.HasMany(u => u.WatchedEpisodes).WithOne(uwe => uwe.User).HasForeignKey(u => u.UserId); ;
         }
     }
 }
