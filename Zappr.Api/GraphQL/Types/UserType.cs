73129a9a8302b85c3caf823e1495df119987f339
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-18 22:12:50 +0200

    Fix: watchlistedseries are not episodes
    duhh

--- Zappr.Api/GraphQL/Types/UserType.cs
+++ Zappr.Api/GraphQL/Types/UserType.cs
@@ -15,7 +15,7 @@ namespace Zappr.Api.GraphQL.Types
             Field(u => u.Email);
 
             // We resolve these ourselves to skip the joined entity
-            Field<ListGraphType<EpisodeType>>("watchListedSeries", "The series this user has watchlisted", resolve: c => c.Source.WatchListedSeries.Select(ue => ue.Series));
+            Field<ListGraphType<SeriesType>>("watchListedSeries", "The series this user has watchlisted", resolve: c => c.Source.WatchListedSeries.Select(ue => ue.Series));
             Field<ListGraphType<SeriesType>>("favoriteSeries", "The series this user has favorited", resolve: c => c.Source.FavoriteSeries.Select(us => us.Series));
             Field<ListGraphType<EpisodeType>>("watchedEpisodes", "The episodes this user has watched", resolve: c => c.Source.WatchedEpisodes.Select(ue => ue.Episode));
             Field<ListGraphType<SeriesType>>("ratedSeries", "The series this user has rated", resolve: c => c.Source.RatedSeries.Select(us => us.Series));
